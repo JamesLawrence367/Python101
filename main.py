9316baeefbc2581f2e0ec355aa601e58a908e380
James Lawrence <441743@edu.cwa.ac.uk>
2023-11-24 10:40:08 +0000

    Commit Two

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # James Lawrence | 24/11/2023
+import datetime
 
 print("Hello, World!")
+
+while True:
+    try:
+        dob = int(input("Enter your year of birth:\n"))
+        break
+    except:
+        print("Enter A Number")
+    
+print("You Are " + str(datetime.date.today().year - dob) + " Years Old!")
